8cab8cf271ce410aa3059ed9ebdc40cc30fa073b
rahulprakasha <53832739+rahulprakasha@users.noreply.github.com>
2021-06-28 00:46:37 +0530

    Add files via upload

--- site2/1.js
+++ site2/1.js
@@ -1,10 +1,20 @@
 function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;var y=document.forms["myForm"]["pass"].value;
+  var x = document.forms["myForm"]["fname"].value;
+  var y = document.forms["myForm"]["pass"].value;
+  var regx_mail = /[a-zA-Z0-9.-]+@(bmsce\.ac\.in)/
   if (x == "") {
     alert("Name must be filled out");
     return false;
   }
-  if(x !="suraj"&& y!="hey"){alert("wrong Email/password");
+  if(y.length<8){
+    alert("Password too short");
+    return false;
+  }
+  if(regx_mail.test(x)){
+    return true;
+  }
+  else{
+    alert("Email not valid");
     return false;
   }
 }
---
+++ site2/connection.php
@@ -0,0 +1,4 @@
+<?php 
+mysql_connect('localhost','root','rahul1999'); 
+mysql_select_db('attendence01'); 
+?>
